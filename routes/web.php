ff70e60f458340e6b630bb0860315641712bd680
Xu jia quan <quannn0180716v@gmail.com>
2020-11-30 21:20:19 +0800

    練習7(設定DELETE的route、修改html表單、設定function)

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::prefix('admin')->group(function () {
     Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');
     Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
     Route::patch('posts/{id}',[AdminPostsController::class, 'update'])->name('admin.posts.update');
+    Route::delete('posts/{id}',[AdminPostsController::class, 'destroy'])->name('admin.posts.destroy');
 });
