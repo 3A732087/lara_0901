f86dc1363766e51f0145ac9ffd883c339c186b35
Xu jia quan <quannn0180716v@gmail.com>
2020-11-30 19:39:32 +0800

    練習3-2顯示查詢到的資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -10,7 +10,7 @@ class AdminPostsController extends Controller
     {
         $posts = Post::orderBy('created_at','DESC')->get();
         $data = [
-            'posts' => $posts,
+            'posts' => $posts
 
         ];
         return view('admin.posts.index',$data);
